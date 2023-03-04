22eca16f567430bdbb1c2f55ba1ae8e51e0ddc90
Anna Kožíšková <koziskoa@natur.cuni.cz>
2023-03-04 17:02:08 +0100

    first commit

---
+++ algorithms.py
@@ -0,0 +1,39 @@
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
+
+# p = qpoint - bod má konstruktor QPoint(X,Y)
+# p.x()
+# p.y()
+#dx = pol[i].x()
+class Algorithms:
+
+    def __init__(self):
+        pass
+
+    def getPointPolygonPositionR(self, q, pol):
+        k = 0 #počet průsečíků
+        n = len(pol) # délka polygonu
+
+        #process all vertices
+        for i in range (n):
+            #reduce coordinates
+            xir = pol[i].x() - q.x()
+            yir = pol[i].y() - q.y()
+
+            xi1r = pol[(i+1)%n].x() - q.x()
+            yi1r = pol[(i+1)%n].y() - q.y()
+            # z běžného seznamu tvoříme circular list - "kruhový seznam"
+
+            #hledání vhodného segmentu - kt je prtnutý hor paprske, oba koncové body jsou v různých polorovinách
+            if(yi1r>0) and (yir<=0) or (yir > 0) and (yi1r <= 0):
+                #computing intersection
+                xm = (xi1r*yir - xir*yi1r)/(yi1r-yir)
+
+                #increment amount of intersections
+                if xm > 0:
+                    k+=1
+        #point is inside
+        if k % 2 == 1:
+            return True
+        return
---
+++ draw.py
@@ -0,0 +1,59 @@
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+
+class Draw(QWidget):
+    def __init__(self, *argsd, **kwargs):
+        super().__init__(*argsd, **kwargs)
+
+        #bude vykreslovat bod a polygon
+        # query point a polygon
+        self.__q = QPointF(0,0)
+        self.__pol = QPolygonF()
+        self.__add_vertex = True #primárně zadává vrchol
+        pass
+
+    def mousePressEvent(self, e:QMouseEvent):
+        #Left mouse button click and its coords
+        x = e.position().x()
+        y = e.position().y()
+
+        #add point to polygon
+        if self.__add_vertex:
+           #create point P se souřadnicemi X a Y
+            p = QPointF(x,y)
+        #append p to polygon (list)
+            self.__pol.append(p)
+        else:
+            self.__q.setX(x)
+            self.__q.setY(y)
+        #repaint screen
+        self.repaint()
+
+    def paintEvent(self, e: QPaintEvent):
+        #draw polygon
+        #create graphic object - vytvoříme nový objekt qpainter
+        qp = QPainter(self)
+
+        #start draw
+        qp.begin(self)
+
+        #set attributes - barva čáry
+        qp.setPen(Qt.GlobalColor.blue)
+        qp.setBrush(Qt.GlobalColor.yellow)
+        qp.drawPolygon(self.__pol)
+        #draw point
+        d = 10
+        qp.drawEllipse(int(self.__q.x()-d/2), int(self.__q.y()-d/2), d, d)
+        qp.end()
+
+    def switchSource(self):
+        #move point or add vertex
+        self.__add_vertex = not(self.__add_vertex)
+
+    def getPoint(self):
+        #get point
+        return  self.__q
+    def getPolygon(self):
+        #get polygon
+        return  self.__pol
